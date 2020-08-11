61bb6d55164dbc58eb1a17433842fca0ea22d5d9
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-11 03:03:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,26 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file)
+  result = {}
+  file = YAML.load_file(file)
+  file.each do |key, value|
+    result[key] = {
+      :english => value[0],
+      :japanese => value[1]
+    }
+  end
+  result
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoji)
+  translator = load_library(file)
+  translator.each do |key, value|
+    if value[:english] == emoji
+      return value[:japanese]
+    end
+  end
+  "Not a valid emoji"
 end
 
 def get_english_meaning
